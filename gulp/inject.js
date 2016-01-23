dfa29486e109555070326f2f70d0a00bb4a319f9
Ash <ashdevfr@gmail.com>
2016-01-23 17:25:42 +0100

    Missing dependency

--- gulp/inject.js
+++ gulp/inject.js
@@ -7,7 +7,7 @@ var conf = require('./conf');
 var $ = require('gulp-load-plugins')();
 
 var wiredep = require('wiredep').stream;
-var _ = require('lodash');
+var _       = require('lodash');
 
 var browserSync = require('browser-sync');
 
@@ -15,23 +15,23 @@ gulp.task('inject-reload', ['inject'], function() {
   browserSync.reload();
 });
 
-gulp.task('inject', ['scripts', 'styles'], function () {
+gulp.task('inject', ['scripts', 'styles'], function() {
   var injectStyles = gulp.src([
     path.join(conf.paths.tmp, '/serve/app/**/*.css'),
     path.join('!' + conf.paths.tmp, '/serve/app/vendor.css')
-  ], { read: false });
+  ], {read : false});
 
   var injectScripts = gulp.src([
-    path.join(conf.paths.src, '/app/**/*.module.js'),
-    path.join(conf.paths.src, '/app/**/*.js'),
-    path.join('!' + conf.paths.src, '/app/**/*.spec.js'),
-    path.join('!' + conf.paths.src, '/app/**/*.mock.js')
-  ])
-  .pipe($.angularFilesort()).on('error', conf.errorHandler('AngularFilesort'));
+      path.join(conf.paths.src, '/app/**/*.module.js'),
+      path.join(conf.paths.src, '/app/**/*.js'),
+      path.join('!' + conf.paths.src, '/app/**/*.spec.js'),
+      path.join('!' + conf.paths.src, '/app/**/*.mock.js')
+    ])
+    .pipe($.angularFilesort()).on('error', conf.errorHandler('AngularFilesort'));
 
   var injectOptions = {
-    ignorePath: [conf.paths.src, path.join(conf.paths.tmp, '/serve')],
-    addRootSlash: false
+    ignorePath   : [conf.paths.src, path.join(conf.paths.tmp, '/serve')],
+    addRootSlash : false
   };
 
   return gulp.src(path.join(conf.paths.src, '/*.html'))
