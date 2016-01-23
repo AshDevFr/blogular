9ab269d495e9fc94d81909b7aa486461dc958c4e
Ash <ashdevfr@gmail.com>
2016-01-23 15:49:22 +0100

    Step-3 solution

---
+++ src/app/index2.controller.js
@@ -0,0 +1,12 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('blogularApp')
+    .controller('Index2Controller', Index2Controller);
+
+  Index2Controller.$inject = ['$scope'];
+
+  function Index2Controller($scope) {
+  }
+})();
