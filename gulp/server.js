f2ae1700453cc61e62d7471e13f0634a5faf652e
Ash <ashdevfr@gmail.com>
2016-01-23 15:33:00 +0100

    Step-1

--- gulp/server.js
+++ gulp/server.js
@@ -1,10 +1,11 @@
 'use strict';
 
-var path = require('path');
-var gulp = require('gulp');
-var conf = require('./conf');
+var path        = require('path');
+var gulp        = require('gulp');
+var runSequence = require('run-sequence');
+var conf        = require('./conf');
 
-var browserSync = require('browser-sync');
+var browserSync    = require('browser-sync');
 var browserSyncSpa = require('browser-sync-spa');
 
 var util = require('util');
@@ -15,34 +16,40 @@ function browserSyncInit(baseDir, browser) {
   browser = browser === undefined ? 'default' : browser;
 
   var routes = null;
-  if(baseDir === conf.paths.src || (util.isArray(baseDir) && baseDir.indexOf(conf.paths.src) !== -1)) {
+  if (baseDir === conf.paths.src || (util.isArray(baseDir) && baseDir.indexOf(conf.paths.src) !== -1)) {
     routes = {
-      '/bower_components': 'bower_components'
+      '/bower_components' : 'bower_components'
     };
   }
 
   var server = {
-    baseDir: baseDir,
-    routes: routes
+    baseDir : baseDir,
+    routes  : routes
   };
 
-  server.middleware = proxyMiddleware('/api', {target: 'http://localhost:3010/', proxyHost: 'localhost:3010/'});
+  server.middleware = proxyMiddleware('/api', {target : 'http://localhost:3010/', proxyHost : 'localhost:3010/'});
 
   browserSync.instance = browserSync.init({
-    startPath: '/',
-    server: server,
-    browser: browser
+    startPath : '/',
+    server    : server,
+    browser   : browser
   });
 }
 
 browserSync.use(browserSyncSpa({
-  selector: '[ng-app]'// Only needed for angular apps
+  selector : '[ng-app]'// Only needed for angular apps
 }));
 
-gulp.task('serve', ['nodemon', 'watch'], function () {
-  browserSyncInit([path.join(conf.paths.tmp, '/serve'), conf.paths.src]);
+gulp.task('serve', function(callback) {
+  runSequence('clean', ['nodemon', 'watch'], function() {
+    browserSyncInit([path.join(conf.paths.tmp, '/serve'), conf.paths.src]);
+    callback();
+  });
 });
 
-gulp.task('serve:dist', ['nodemon', 'build'], function () {
-  browserSyncInit(conf.paths.dist);
+gulp.task('serve:dist', function(callback) {
+  runSequence('clean', ['nodemon', 'watch'], function() {
+    browserSyncInit(conf.paths.dist);
+    callback();
+  });
 });
