2b2404ed9e764821c198d15497d3f539d1bfb214
Ash <ashdevfr@gmail.com>
2016-01-23 18:27:02 +0100

    Step-6 solution

--- src/app/articles/newArticle/newArticle.controller.js
+++ src/app/articles/newArticle/newArticle.controller.js
@@ -13,7 +13,13 @@
     $scope.addArticle = addArticle;
 
     function addArticle() {
+      var date = new Date();
+      $scope.newArticle.published = date;
+
       console.log($scope.newArticle);
+
+      $scope.articles.push($scope.newArticle);
+      $scope.newArticle = {};
     }
   }
 })();
