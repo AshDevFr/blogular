5131132c1f0b54e2af682670ec77c3d744ee19aa
Ash <ashdevfr@gmail.com>
2016-01-27 12:00:46 +0100

    Add gulp-watch

--- gulp/watch.js
+++ gulp/watch.js
@@ -1,8 +1,9 @@
 'use strict';
 
-var path = require('path');
-var gulp = require('gulp');
-var conf = require('./conf');
+var path  = require('path');
+var gulp  = require('gulp');
+var watch = require('gulp-watch');
+var conf  = require('./conf');
 
 var browserSync = require('browser-sync');
 
@@ -10,30 +11,30 @@ function isOnlyChange(event) {
   return event.type === 'changed';
 }
 
-gulp.task('watch', ['inject'], function () {
+gulp.task('watch', ['inject'], function() {
 
-  gulp.watch([path.join(conf.paths.src, '/*.html'), 'bower.json'], ['inject-reload']);
+  watch([path.join(conf.paths.src, '/*.html'), 'bower.json'], ['inject-reload']);
 
-  gulp.watch([
+  watch([
     path.join(conf.paths.src, '/app/**/*.css'),
     path.join(conf.paths.src, '/app/**/*.scss')
   ], function(event) {
-    if(isOnlyChange(event)) {
+    if (isOnlyChange(event)) {
       gulp.start('styles-reload');
     } else {
       gulp.start('inject-reload');
     }
   });
 
-  gulp.watch(path.join(conf.paths.src, '/app/**/*.js'), function(event) {
-    if(isOnlyChange(event)) {
+  watch(path.join(conf.paths.src, '/app/**/*.js'), function(event) {
+    if (isOnlyChange(event)) {
       gulp.start('scripts-reload');
     } else {
       gulp.start('inject-reload');
     }
   });
 
-  gulp.watch(path.join(conf.paths.src, '/app/**/*.html'), function(event) {
+  watch(path.join(conf.paths.src, '/app/**/*.html'), function(event) {
     browserSync.reload(event.path);
   });
 });
