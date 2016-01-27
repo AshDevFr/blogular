91f4be95f68244eb384f8f5ffb40dcf9de7e9bbb
Ash <ashdevfr@gmail.com>
2016-01-27 13:51:02 +0100

    Remove interval in the task watch

--- gulp/watch.js
+++ gulp/watch.js
@@ -7,39 +7,51 @@ var conf  = require('./conf');
 
 var browserSync = require('browser-sync');
 
+function isOnlyChange(event) {
+  return event.type === 'change';
+}
+
 gulp.task('watch', ['inject'], function() {
   watch([path.join(conf.paths.src, '/*.html'), 'bower.json'],
     {
-      usePolling : process.env.POLL || false,
-      interval   : 1000
+      usePolling : process.env.POLL || false
     },
     function() {
       gulp.start('inject-reload');
     });
 
   watch([
-    path.join(conf.paths.src, '/app/**/*.css'),
-    path.join(conf.paths.src, '/app/**/*.scss')
-  ], {
-    usePolling : process.env.POLL || false,
-    interval   : 1000
-  }, function() {
-    gulp.start('styles-reload');
-  });
+      path.join(conf.paths.src, '/app/**/*.css'),
+      path.join(conf.paths.src, '/app/**/*.scss')
+    ],
+    {
+      usePolling : process.env.POLL || false
+    },
+    function(file) {
+      if (isOnlyChange(file.event)) {
+        gulp.start('styles-reload');
+      } else {
+        gulp.start('inject-reload');
+      }
+    });
 
   watch(path.join(conf.paths.src, '/app/**/*.js'),
     {
-      usePolling : process.env.POLL || false,
-      interval   : 1000
-    }, function() {
-      gulp.start('scripts-reload');
+      usePolling : process.env.POLL || false
+    },
+    function(file) {
+      if (isOnlyChange(file.event)) {
+        gulp.start('styles-reload');
+      } else {
+        gulp.start('inject-reload');
+      }
     });
 
   watch(path.join(conf.paths.src, '/app/**/*.html'),
     {
-      usePolling : process.env.POLL || false,
-      interval   : 1000
-    }, function() {
-      browserSync.reload();
+      usePolling : process.env.POLL || false
+    },
+    function(file) {
+      browserSync.reload(file.path);
     });
 });
