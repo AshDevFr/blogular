73ed61da9687937550cb29d3f3ecfe856b43eb77
Sylvain CAU <ashdevfr@gmail.com>
2016-03-08 15:47:36 +0100

    Server: enable CORS

--- server/server.js
+++ server/server.js
@@ -14,6 +14,12 @@ var express    = require('express'),
 app.set('port', port);
 app.use(bodyParser());
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 //app.use(methodOverride());
 
 app.get('/api/articles', article.findAll);
